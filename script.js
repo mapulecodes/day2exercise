78bd50ba4de47ef4ccf4c92dc6c208f9cd753247
mapulecodes <mbuzwanamapule@gmail.com>
2024-05-14 15:34:57 +0200

    mapoolah

---
+++ script.js
@@ -0,0 +1,18 @@
+let firstName = "mapule"
+console.log(firstName)
+let lastName = "mbuzwana"
+console.log(lastName)
+let numberVar = 5
+console.log(numberVar*5)
+const nameOne = "mapule"
+nameOne = "mbuzwana"
+
+
+
+
+
+
+
+
+
+
